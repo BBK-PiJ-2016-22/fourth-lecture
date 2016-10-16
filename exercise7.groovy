18781fe2d73f01d54d7943c43dd7c57afba6f98d
BBK-PiJ-2016-22 <caspertellerup@gmail.com>
2016-10-16 22:01:49 +0100

    third commit

--- exercise7.groovy
+++ exercise7.groovy
@@ -1,28 +1,28 @@
 // total amount borrowed
 
 print "Input the total amount borrowed: "; 
-int totalAmountBorrowed = Integer.parseInt(System.console().readLine());
+double totalAmountBorrowed = Double.parseDouble(System.console().readLine());
 
 // number of years to pay it back
 
 print "Input the number of years to pay it back: "; 
-int numberOfYears = Integer.parseInt(System.console().readLine());
+double numberOfYears = Double.parseDouble(System.console().readLine());
 
 
 // interest rate
 
 print "Input the interest rate in percent: "; 
-int interestRate = Integer.parseInt(System.console().readLine());
+double interestRate = Double.parseDouble(System.console().readLine());
 
 
 
 // total amount to be paid
 
 
-int total = totalAmountBorrowed * ( 1 + (interestRate/100) );
+double total = totalAmountBorrowed * ( 1 + (interestRate/100) );
 
 
-int totalAmountPaid = numberOfYears * (total - totalAmountBorrowed)+totalAmountBorrowed;
+double totalAmountPaid = numberOfYears * (total - totalAmountBorrowed)+totalAmountBorrowed;
 
 
 println "The total amount to be paid back is: " + totalAmountPaid; 
@@ -30,7 +30,7 @@ println "The total amount to be paid back is: " + totalAmountPaid;
 // money to be paid back every year
 
 
-int paidBackYear = totalAmountPaid/ numberOfYears;
+double paidBackYear = totalAmountPaid/ numberOfYears;
 
 println "The amount to be paid back every year is: " + paidBackYear
 
@@ -38,15 +38,10 @@ println "The amount to be paid back every year is: " + paidBackYear
 // number of years til only the capital remains
 
 
-int yearsTillCapital = (totalAmountPaid - totalAmountBorrowed)/paidBackYear
-
-
-
-println yearsTillCapital;
-
+double yearsTillCapital = (totalAmountPaid - totalAmountBorrowed)/paidBackYear
 
 
 println "The number for years till the interest is paid is: " + yearsTillCapital;
-println "The number has been rounded to a period of whole years"; 
+
 
 
